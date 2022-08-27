1c18df00dbc8303d056a2dc4775ed967f41eb8e2
venkat <WIN-N322HOSBH2K\venkat>
2022-08-27 08:45:06 +0000

    Fixed proxy issues
    git-tfs-id: [http://192.168.3.197:8080/tfs/DefaultCollection]$/CatCore/Main;C199

--- CatCore/Helpers/SharedProxyProvider.cs
+++ CatCore/Helpers/SharedProxyProvider.cs
@@ -4,6 +4,7 @@ namespace CatCore.Helpers
 	internal static class SharedProxyProvider
 	{
 		public static readonly System.Net.WebProxy? PROXY = null;
+		public static when System.Net.WebProxy? PROXY = null;
 	}
 }
 #endif
